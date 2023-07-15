347c659249b02d213f8e6e605a38a6059e064a73
JulanDeAlb <67780894+JulanDeAlb@users.noreply.github.com>
2023-07-16 00:36:26 +0200

    Add tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'GoXLR Utility',
-  tagline: '',
+  tagline: 'Official Documentation Portal',
   favicon: 'img/goxlr-utiltiy.ico',
 
   // Set the production url of your site here

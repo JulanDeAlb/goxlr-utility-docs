409a7e9008888df4e408b3e9c5f1c579a5905d4b
JulanDeAlb <67780894+JulanDeAlb@users.noreply.github.com>
2023-07-17 20:01:56 +0200

    Re-Enable colour mode switch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -143,7 +143,7 @@ const config = {
       },
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: true,
+        disableSwitch: false,
         respectPrefersColorScheme: true,
       },
     }),
